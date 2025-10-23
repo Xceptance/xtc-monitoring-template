ca4b27ebba2ef4bc7f8c1711b72e8b7e22429505
Jan Robert Avemarg <jan.robert.avemarg@xceptance.com>
2025-10-23 14:56:50 +0200

    improve warmup

--- src/test/java/company/util/WarmUpFlow.java
+++ src/test/java/company/util/WarmUpFlow.java
@@ -1,24 +1,28 @@
 package company.util;
 
-import static com.xceptance.xlt.api.engine.scripting.StaticScriptCommands.open;
-import static com.xceptance.xlt.api.engine.scripting.StaticScriptCommands.pause;
-import static com.xceptance.xlt.api.engine.scripting.StaticScriptCommands.startAction;
-import static com.xceptance.xlt.api.engine.scripting.StaticScriptCommands.stopAction;
-
+import com.xceptance.xlt.api.engine.ActionData;
 import com.xceptance.xlt.api.engine.Session;
 import com.xceptance.xlt.api.util.XltProperties;
 import com.xceptance.xlt.engine.scripting.TestContext;
 import com.xceptance.xlt.engine.scripting.webdriver.WebDriverScriptCommands;
 
+import static com.xceptance.xlt.api.engine.scripting.StaticScriptCommands.open;
+import static com.xceptance.xlt.api.engine.scripting.StaticScriptCommands.pause;
+import static com.xceptance.xlt.api.engine.scripting.StaticScriptCommands.startAction;
+import static com.xceptance.xlt.api.engine.scripting.StaticScriptCommands.stopAction;
+
 public class WarmUpFlow
 {
     /**
-     * Warm up for browserless tests like {@code TPing}</br>
-     * Just some warm up code that ensure we measure right but don't break things
+     * Warm up for browserless tests like {@code TPing}</br> Just some warm up code that ensure we measure right but don't break things
      */
     public static void headlessWarmup()
     {
         // I am just here to get the VM warm
+
+        // get warmup start system time
+        long startTime = System.currentTimeMillis();
+
         try
         {
             Session.getCurrent().getDataManager().setLoggingEnabled(false);
@@ -26,8 +30,7 @@ public class WarmUpFlow
             // warm up
             for (int i = 0; i < 4; i++)
             {
-                new LoadUrlLightWeightAction("HiddenWarmUp", 
-                                             XltProperties.getInstance().getProperty("xlt.warmUpUrl")).run();
+                new LoadUrlLightWeightAction("HiddenWarmUp", getWarmupUrl()).run();
             }
         }
         catch (final Throwable e)
@@ -38,12 +41,21 @@ public class WarmUpFlow
         {
             // bring logging back
             Session.getCurrent().getDataManager().setLoggingEnabled(true);
+
+            // get warmup end system time
+            long endtime = System.currentTimeMillis();
+
+            ActionData warmupAction = new ActionData("Warmup - Ping");
+            warmupAction.setTime(startTime);
+            warmupAction.setRunTime(endtime - startTime);
+
+            // log the warmup time
+            Session.getCurrent().getDataManager().logDataRecord(warmupAction);
         }
     }
 
     /**
-     * Warm up for browser tests like {@code THomepage}</br>
-     * Just some warm up code that ensure we measure right but don't break things
+     * Warm up for browser tests like {@code THomepage}</br> Just some warm up code that ensure we measure right but don't break things
      */
     public static void warmup()
     {
@@ -54,7 +66,7 @@ public class WarmUpFlow
             // don't log this
             Session.getCurrent().getDataManager().setLoggingEnabled(false);
             startAction("Warmup");
-            open(XltProperties.getInstance().getProperty("xlt.warmUpUrl"));
+            open(getWarmupUrl());
             pause(2000);
             stopAction();
         }
@@ -70,5 +82,10 @@ public class WarmUpFlow
             // bring back logging
             Session.getCurrent().getDataManager().setLoggingEnabled(true);
         }
+    }
+
+    private static String getWarmupUrl()
+    {
+        return XltProperties.getInstance().getProperty("xlt.ping.warmup.url", "https://www.google.com/");
     }
 }
